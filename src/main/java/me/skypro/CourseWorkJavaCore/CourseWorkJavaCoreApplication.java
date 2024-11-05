9376e6e4e569b7d49e058c88732cecd9a8cd8c97
Leysen95 <mullayanova95@bk.ru>
2024-11-05 18:42:27 +0500

    initial commit

---
+++ src/main/java/me/skypro/CourseWorkJavaCore/CourseWorkJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package me.skypro.CourseWorkJavaCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkJavaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkJavaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/skypro/CourseWorkJavaCore/CourseWorkJavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.skypro.CourseWorkJavaCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkJavaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
